b877479412e7e725f47458c8e4d58f744aa655d1
Sierra-Soto <62355285+Sage-tech@users.noreply.github.com>
2021-06-07 14:22:41 -0700

    update

---
+++ C# Obejcts - Instantiation/Point.cs
@@ -0,0 +1,16 @@
+namespace Defense 
+{
+    class { Point}
+    {
+        public readonly int X;
+        public readonly int Y;
+
+        public Point(int x, int y)
+        {
+    X = x;
+    Y = y;
+        }
+
+    }
+
+}
