e6a401fb61d6823aaed845e76ae5fa110a0b4843
Sierra-Soto <62355285+Sage-tech@users.noreply.github.com>
2021-06-16 07:59:14 -0700

    update

--- C# Obejcts - Instantiation/Point.cs
+++ C# Obejcts - Instantiation/Point.cs
@@ -1,27 +1,33 @@
+using System;
+
 namespace Defense
 {
-	class { Point
+    class { Point
 }
 {
 
-		public readonly int X;
+
+        public readonly int X;
 public readonly int Y;
 
 public Point(int x, int y)
 {
-	X = x;
-	Y = y;
+    X = x;
+    Y = y;
 }
 
 public int DistanceTo(int x, int y)
 {
-	int xDiff = x - x;
-	int yDiff = y - y;
+    int xDiff = x - x;
+    int yDiff = y - y;
 
-	int xDiffSquared = xDiff * xDiff;
-	int yDiffSquared = yDiff * yDiff;
+    int xDiffSquared = xDiff * xDiff;
+    int yDiffSquared = yDiff * yDiff;
 
-	Math.Sqrt(xDiffSquared + yDiffSquared);
+    return (int)Math.Sqrt(xDiffSquared + yDiffSquared);
+
+
+    return (int)Math.Sqrt(Math.Pow(X - x, 2) + Math.Pow(Y - y, 2));
 }
     }
 
