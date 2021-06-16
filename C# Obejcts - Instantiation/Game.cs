79d9b41f635cddd1759260d2678bacf5cdd00dc6
Sierra-Soto <sierra_soto@uhaul.com>
2021-06-16 13:27:30 -0700

    update

--- C# Obejcts - Instantiation/Game.cs
+++ C# Obejcts - Instantiation/Game.cs
@@ -7,12 +7,29 @@ namespace Defense
         public static void Main()
         {
             Map map = new Map(8, 5);
+            try
+            {
+                MapLocation mapLocation = new MapLocation(20, 20, map);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("That map lcoation is not on the map");
 
-            Point point = new Point(4, 2);
+                }
 
-            Console.WriteLine(point.DistanceTo(5, 5));
+           //Point x = new MapLocation(4, 2);
 
+           // Point p = x;
 
+           // map.OnMap(new MapLocation(0, 0));
+
+           // Console.WriteLine(x.DistanceTo(5, 5));
+
+           // Console.WriteLine(x is MapLocation);
+           // Console.WriteLine(x is Point);
+
+           // Point point = new Point(0, 0);
+           // Console.WriteLine(point is MapLocation);
 
         }
     }
---
+++ C# Obejcts - Instantiation/MapLocation.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Denfense
+{
+    class MapLocation : Point
+
+    {
+        public MapLocation(int x, int y, Map map) : base(5, 5);
+        { 
+        if (!map.OnMap(this))
+            {
+            throw new System.Exception();
+        }
+        }
+    }
+}
